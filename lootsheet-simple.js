8cf7a6b788df4b93a9a00018b4d7a167c62a6b40
Brandon Coston <costonb@gmail.com>
2023-07-08 14:25:12 -0700

    Update lootsheet-simple.js
    Adjust logic to merge destination items if they exist

--- lootsheet-simple.js
+++ lootsheet-simple.js
@@ -1483,18 +1483,17 @@ Hooks.once("init", () => {
         item: newItem,
         quantity: quantity,
       });
-      /* let destItem = destination.data.items.find(i => i.name == newItem.name);
-			// console.log("DESTITEM: \n"); 
-			// console.log(destItem); */
-      additions.push(newItem);
-      /* if (destItem === undefined) {
-                additions.push(newItem);
-            } else {
-                // console.log("Existing Item");
-				newItem.data.quantity = Number(destitem.system.quantity) + Number(newItem.data.quantity);
-				additions.push(newItem);
-				
-            } */
+      let destItem = destination.items.find(i => i.name == newItem.name);
+      // console.log("DESTITEM: \n"); 
+      // console.log(destItem); */
+      if (destItem === undefined) {
+        additions.push(newItem);
+      } else {
+        // console.log("Existing Item");
+        let updateItem = duplicate(destItem);
+        updateItem.system.quantity = Number(destItem.system.quantity) + Number(newItem.system.quantity);
+        destUpdates.push(updateItem);
+      }
     }
 
     if (deletes.length > 0) {
